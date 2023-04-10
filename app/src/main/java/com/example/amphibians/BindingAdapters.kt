3e5c46679bd5499e0de3455bbe90276bc5c10cc9
Karine Antoniacomi <karineas@pbtech.net.br>
2023-04-10 12:15:04 -0300

    feat: Implementa ViewModel para enviar solicitações à API e armazenar valores à serem exibidos

--- app/src/main/java/com/example/amphibians/BindingAdapters.kt
+++ app/src/main/java/com/example/amphibians/BindingAdapters.kt
@@ -32,12 +32,10 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Amphibian>?) {
     adapter.submitList(data)
 }
 
-/**
- * This binding adapter displays the [AmphibianApiStatus] of the network request in an image view.
- * When the request is loading, it displays a loading_animation.  If the request has an error, it
- * displays a broken image to reflect the connection error.  When the request is finished, it
- * hides the image view.
- */
+/** This binding adapter displays the [AmphibianApiStatus] of the network request in an
+ * image view. When the request is loading, it displays a loading_animation.  If the
+ * request has an error, it displays a broken image to reflect the connection error.
+ * When the request is finished, it hides the image view. */
 @BindingAdapter("apiStatus")
 fun bindStatus(statusImageView: ImageView, status: AmphibianApiStatus?) {
     when(status) {
--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,57 @@
  */
 package com.example.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
 
 enum class AmphibianApiStatus {LOADING, ERROR, DONE}
 
 class AmphibianViewModel : ViewModel() {
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    // Properties to represent MutableLiveData and LiveData for the API status
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus> = _status
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    // Properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibians = MutableLiveData<List<Amphibian>>()
+    val amphibians : LiveData<List<Amphibian>> = _amphibians
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
-    //  This will be used to display the details of an amphibian when a list item is clicked
+    // Properties to represent MutableLiveData and LiveData for a single amphibian object.
+    // This will be used to display the details of an amphibian when a list item is clicked
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian : LiveData<Amphibian> = _amphibian
 
-    // TODO: Create a function that gets a list of amphibians from the api service and sets the
-    //  status via a Coroutine
+    // Function that gets a list of amphibians from the api service and sets the status via a Coroutine
+    private fun getAmphibiansList() {
+        // inicia corrotina para executar solicitação GET e fazer download dos dados
+        // de anfíbios chamando o método getAmphibians() do serviço da Retrofit.
+        viewModelScope.launch {
+            // status inicial da corrotina
+            _status.value = AmphibianApiStatus.LOADING
+            // verifica se ocorre excessão ao enviar a solicitação GET
+            try {
+                _amphibians.value = AmphibianApi.retrofitService.getAmphibians()
+                _status.value = AmphibianApiStatus.DONE
 
+            // trata excessão
+            } catch (e: Exception) {
+                _status.value = AmphibianApiStatus.ERROR
+                // Define como lista vazia
+                _amphibians.value = listOf()
+            }
+        }
+    }
+
+    // Configura a propriedade _amphibian definida para o argumento do anfíbio transmitido à função.
+    // Esse método é chamado ao selecionar um anfíbio, para que seja exibido na tela de detalhes.
     fun onAmphibianClicked(amphibian: Amphibian) {
-        // TODO: Set the amphibian object
+        // Set the amphibian object
+        // anfíbio clicaco -> chama onAmphibianClicked() e passa o objeto do anfíbio para ele.
+        _amphibian.value = amphibian
     }
 }
